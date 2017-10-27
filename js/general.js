66bde6612ad9028a06d4c1184d7ae67647e45ee1
Tusa Larkowski <rpi.larkot@gmail.com>
2017-10-27 11:26:34 -0400

    Adding General Files to Initial Folders

---
+++ js/general.js
@@ -0,0 +1,7 @@
+/*
+	Application: CMK Marketing Customer Management System
+	Module: General Scripting
+
+	Developers: Tusa Larkowski, Adeel Minhas, Ruowen Qin
+	Brief Description: Handles any general functions for the entire application.
+*/
