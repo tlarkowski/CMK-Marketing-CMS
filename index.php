e70eeb790574bf2a0fb619abb107d0007e60bc68
Tusa Larkowski <rpi.larkot@gmail.com>
2017-11-11 14:51:36 -0500

    Auto stash before merge of "master" and "origin/master"

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
 	<!-- Navigation -->
 	<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
 		<div class="container">
-			<a class="navbar-brand" href="#">Welcome Caitlin</a>
+			<p class="nav-link">Welcome Caitlin!</p>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
