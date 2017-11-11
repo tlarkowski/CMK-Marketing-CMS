90961d3150a346ef48f8f3df9fcccb0d915314d0
Tusa Larkowski <rpi.larkot@gmail.com>
2017-11-10 23:23:13 -0500

    example of just copying code from blank template

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
 
 <!-- Page Content -->
 <body>
-	<?php
+	<!-- <?php
 
 	if ($logged_in) {
 		echo "<h1>User is logged in</h1>";
@@ -35,7 +35,34 @@
 		echo "<h1>Please log in</h1>";
 	}
 
-	?>
+	?> -->
+
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">Start Bootstrap</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
 </body>
 
 </html>
