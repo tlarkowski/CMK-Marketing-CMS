d647fb976c2f0224a68a17f3b52e62d8dba5b098
Tusa Larkowski <rpi.larkot@gmail.com>
2017-11-11 15:58:32 -0500

    Also added navigation to Cleint Info Page

--- index.php
+++ index.php
@@ -28,18 +28,8 @@
 <body>
 	<?php include '/include/navbar.html';?>
 
-	<!-- Page Content -->
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-12 text-center">
-				<h1 class="mt-5">A Bootstrap 4 Starter Template</h1>
-				<p class="lead">Complete with pre-defined file paths and responsive navigation!</p>
-				<ul class="list-unstyled">
-					<li>Bootstrap 4.0.0-beta</li>
-					<li>jQuery 3.2.1</li>
-				</ul>
-			</div>
-		</div>
+	<div style="margin-top: 60px;">
+		<a href="/pages/client-info"><h2>Client Info</h2></a>
 	</div>
 </body>
 
