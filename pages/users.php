fa5f32502b48728cb9815bc2e4cec8f6456b1931
Tusa Larkowski <rpi.larkot@gmail.com>
2017-11-11 19:55:32 -0500

    Added Link to main.css in All Pages

--- pages/users.php
+++ pages/users.php
@@ -14,6 +14,9 @@
 	<meta charset="UTF-8">
 	<title>Client Management Homepage</title>
 
+	<!-- Personal CSS -->
+	<link rel="stylesheet" href="/css/main.css">
+
 	<!-- General CDN Additions -->
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 
