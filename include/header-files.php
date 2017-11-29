50146cd238b2db9d7b157cfd93148e9542230bf1
Tusa Larkowski <rpi.larkot@gmail.com>
2017-11-29 08:29:06 -0500

    Changing jQuery version Loaded

--- include/header-files.php
+++ include/header-files.php
@@ -9,9 +9,7 @@
 <link rel="shortcut icon" href="/favicon.ico">
 
 <!-- General CDN Additions -->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-        crossorigin="anonymous"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js" crossorigin="anonymous"></script>
 
 <!-- Bootstrap CDN Additions -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css"
