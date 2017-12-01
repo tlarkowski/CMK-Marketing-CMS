c09a86f1b7d41c41807c857698a6baf889a1d6ce
chinrw <chinqrw@gmail.com>
2017-12-01 17:53:35 -0500

    Add weeks ago function

--- clients/searchProject.php
+++ clients/searchProject.php
@@ -5,13 +5,14 @@
  * Date: 2017/11/15
  * Time: 11:01
  */
+date_default_timezone_set("EST");
 
 require_once $_SERVER["DOCUMENT_ROOT"] . "/db/conn.php";
 
 /* Return all projects in database */
 function all_projects()
 {
-	$data_conn = connection();
+    $data_conn = connection();
     $data = $data_conn->select("Client_Project", "*", [
         "Status" => "1"
     ]);
@@ -57,5 +58,21 @@ function all_project_client_info($project)
         "Company_ID" => $project['Company_ID'],
         "Status" => "1"
     ]);
+    return $data;
+}
+
+function search_project_two_weeks_within()
+{
+    $data_conn = connection();
+
+    $two_weeks_ago = date("Y-m-d H:i:s", strtotime("-14 day"));
+//    $two_weeks_ago = date("Y-m-d H:i:s", strtotime("-1 month"));
+//    $two_weeks_ago = date("Y-m-d H:i:s", strtotime("-1 week"));
+//    echo $two_weeks_ago = date("Y-m-d H:i:s", strtotime("-2 day -1 week"));
+
+    $data = $data_conn->select("Client_Project", "*", [
+        "End_Date[>=]" => $two_weeks_ago
+    ]);
+
     return $data;
 }
